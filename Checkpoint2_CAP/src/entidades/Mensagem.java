df318d6a327d1b9a7fc988a56cc372c034b6ef4f
Guilherme Mendes da Cunha <60897048+firgo12@users.noreply.github.com>
2023-04-24 14:37:01 -0300

    Mudando String aux para vazia
    String aux = "";

--- Checkpoint2_CAP/src/entidades/Mensagem.java
+++ Checkpoint2_CAP/src/entidades/Mensagem.java
@@ -23,7 +23,7 @@ public class Mensagem {
 	@Override
 	public String toString() {
 		String aux = "";
-		aux = "Nome: "+ nome + "\n";
+		aux += "Nome: "+ nome + "\n";
 		aux += "Email/Telefone: "+ emailTelefone + "\n";
 		aux += "Motivo de Contato: "+ motivoContato + "\n";
 		aux += "Mensagem: "+ mensagem;
